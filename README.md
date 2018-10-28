3dfaf0f168f40d0cb8b267eba11f3bc0eb8f1b93
bribrah <cheesemas46@gmail.com>
2018-10-28 16:32:37 -0700

    added a lot of functionality, nearing completetion

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # A project to create a calculator with a graphical user interface
+## Current version of project can be found [here](https://bribrah.github.io/calculator/)
--- main.js
+++ main.js
@@ -5,10 +5,13 @@ function operate(num1,num2,operation){
     else if (operation == "-"){
         return num1-num2;
     }
-    else if (operation == "X"){
+    else if (operation == "x"){
     return num1*num2;
     }
     else if (operation == "/"){
+        if (num2 == 0){
+            return "Burn in hell!";
+        }
         quotient = num1 / num2
         return quotient;
     }
@@ -18,6 +21,7 @@ function operate(num1,num2,operation){
 let screen = document.querySelector(".screen");
 let numberButtons = document.querySelectorAll(".number")
 let operatorButtons = document.querySelectorAll('.operator');
+let topScreen = document.querySelector(".topScreen");
 
 let screenInput = 0;
 
@@ -27,7 +31,10 @@ function inputNumber(){
         screen.textContent = ""
     }
     screen.textContent += this.innerHTML;
+    topScreen.textContent += this.innerHTML;
     screenInput += this.innerHTML;
+    operatorEnteredLast = false;
+    
 }
 
 let num1 = 0;
@@ -35,14 +42,15 @@ let num2 = 0;
 let result = 0;
 let operator = ""
 let operatorEntered = false;
+let operatorEnteredLast = false;
 function pressOperate(){
     if (this.innerHTML == "="){
         if (operatorEntered == true){
-            num2 = parseInt(screenInput);
+            num2 = +screenInput;
             result = operate(num1,num2,operator)          
         }
         else{
-            result = screen.innerHTML;
+            result = screen.textContent;
             console.log(result)
         }
         console.log("num1:" + num1);
@@ -54,26 +62,45 @@ function pressOperate(){
         num1 = 0;
         num2 = 0;
         screenInput = "";
-        operator = ""
+        operator = "";
+        topScreen.textContent = ""
+    }
+    else if(topScreen.textContent == "" || operatorEnteredLast == true){
+        return;
+    }
+    else if (this.innerHTML == "clear"){
+        screen.textContent = ""
+        operatorEntered = false;
+        num1 = 0;
+        num2 = 0;
+        screenInput = "";
+        operator = "";
+        topScreen.textContent = "";
     }
     else if (operatorEntered == true || num1 !=0){
-        num2=parseInt(screenInput);
+        num2=+screenInput;
+        console.log("num1:" + num1);
+        console.log("num2: " + num2);
+        console.log("operator: " + operator);
         result = operate(num1,num2,operator);
         console.log(result);
         screen.textContent = result;
+        
         num1 = result;
         num2= 0;
         screenInput = 0;
-        operator = '';
-        operatorEntered = false;
-    }
+        operator = this.innerHTML;
+        topScreen.textContent = result + operator;
+}
     else{
-        num1 = parseInt(screenInput);;
+        num1 = +screenInput;
         screenInput = "";
         screen.textContent = "";
         operator = this.innerHTML;
         operatorEntered = true;
+        topScreen.textContent += this.innerHTML;
     }
+    operatorEnteredLast = true;
 }
     
 
